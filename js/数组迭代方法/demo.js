e613d81e464dbc32c4c5979867d96efe22d58889
zhourusheng <nfuzrs@126.com>
2019-08-08 10:44:06 +0800

    数组迭代方法

--- js/数组迭代方法/demo.js
+++ js/数组迭代方法/demo.js
@@ -1,7 +1,41 @@
-// forEach:
 var color = ['red', 'blue', 'green']
 
-// ES5
+// ES5：for循环数组遍历
 for(var i = 0; i < color.length; i++) {
   console.log(color[i])
 }
+
+// forEach: 数组遍历
+// callback()接收三个参数
+color.forEach((item, index, array) => {
+  console.log(item, index, array)
+})
+
+
+// 网红面试题： 
+console.log(['1', '2', '3'].map(parseInt))
+// 你可能觉的会是[1, 2, 3]
+// 但实际的结果是 [1, NaN, NaN]
+
+// 通常使用parseInt时,只需要传递一个参数.
+// 但实际上,parseInt可以有两个参数.第二个参数是进制数.
+// 可以通过语句"alert(parseInt.length)===2"来验证.
+// map方法在调用callback函数时,会给它传递三个参数:当前正在遍历的元素, 
+// 元素索引, 原数组本身.
+// 第三个参数parseInt会忽视, 但第二个参数不会,也就是说,
+// parseInt把传过来的索引值当成进制数来使用.从而返回了NaN.
+
+function returnInt(element) {
+  return parseInt(element, 10)
+}
+
+console.log(['1', '2', '3'].map(returnInt)) // [1, 2, 3]
+// 意料之中的结果
+
+// 也可以使用简单的箭头函数，结果同上
+['1', '2', '3'].map( str => parseInt(str) );
+
+// 一个更简单的方式:
+['1', '2', '3'].map(Number); // [1, 2, 3]
+// 与`parseInt` 不同，下面的结果会返回浮点数或指数:
+['1.1', '2.2e2', '3e300'].map(Number); // [1.1, 220, 3e+300]
