a2c74972a6b5ba96c2d236d8e0459e4c02801ac6
zhourusheng <nfuzrs@126.com>
2020-01-10 17:55:20 +0800

    ts基础类型

--- ts/index.js
+++
@@ -1,8 +0,0 @@
-function greeter(person) {
-    return "hello, " + person.firstName + " " + person.lastName;
-}
-var user = {
-    firstName: 'zhou',
-    lastName: 'rusheng'
-};
-console.log(greeter(user));
