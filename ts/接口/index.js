541f6b07e79319b23e84623b9f4bae99d18adb63
zhourusheng <nfuzrs@126.com>
2020-01-13 15:58:52 +0800

    接口静态类型检查

---
+++ ts/接口/index.js
@@ -0,0 +1,15 @@
+function createSuare(config) {
+    var newSquare = {
+        color: 'white',
+        area: 100
+    };
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+var mySquare = createSuare({ color: 'red' });
+console.log(mySquare);
