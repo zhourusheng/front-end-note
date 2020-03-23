8cad1cead1a70f779cabc97b2bf6b8678bb73f23
zhourusheng <nfuzrs@126.com>
2020-03-23 16:46:45 +0800

    垃圾回收

--- js-basic/原型和原型链/deepLearn.js
+++ js-basic/原型和原型链/deepLearn.js
@@ -7,9 +7,11 @@ Person.prototype.name = 'zhourusheng'
 var person1 = new Person()
 var person2 = new Person()
 
-console.log(person1.name)
-console.log(person2.name)
+console.log(person1.name) // zhourusheng
+console.log(person2.name) // zhourusheng
 
 
+// 每个函数都有一个 prototype 属性
+// 什么是原型？ 每一个JavaScript 对象（除了null）在创建时就会关联另一个对象，这个对象就是原型，每个对象都会从原型 “继承” 属性
 
 // prototype 和 __proto__ 的关系
